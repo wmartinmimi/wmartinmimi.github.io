de586995a410a9aaeffad9ef57e0f44dd4e5f55b
wmartinmimi <wmartinmimipub@gmail.com>
2024-10-06 22:23:14 +0100

    update to remove bloat and add navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://martinmimi.w10.site',
-    output: 'static'
+    output: 'static',
 });
