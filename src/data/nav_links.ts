86a1dc23f07c909bdefdd9683929600b8f554d52
wmartinmimi <wmartinmimipub@gmail.com>
2024-10-20 23:00:56 +0100

    dynamic nav page links filling

---
+++ src/data/nav_links.ts
@@ -0,0 +1,14 @@
+const nav_links = [
+    {
+        title: "Projects",
+        link: "/projects",
+    },
+    {
+        title: "Tools I use",
+        link: "/tools",
+    },
+];
+
+export {
+    nav_links
+};
