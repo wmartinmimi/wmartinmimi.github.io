5b3b9677ee889834b05019eacedc34318b76d18b
wmartinmimi <wmartinmimipub@gmail.com>
2024-11-09 12:21:31 +0000

    add canonical link generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://martinmimi.w10.site',
+    site: 'https://wmartinmimi.github.io',
     output: 'static',
 });
