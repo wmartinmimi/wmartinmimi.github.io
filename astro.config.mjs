6349dbe59fee6a07785bc7c57b4de20df4e367fb
wmartinmimi <wmartinmimipub@gmail.com>
2025-01-17 22:32:29 +0000

    update website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wmartinmimi.github.io',
+    site: 'https://martinmimi.pages.dev',
     output: 'static',
     outDir: 'docs',
     markdown: {
